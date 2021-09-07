0d581d268a68405f8a9a67196e8194507323a420
Tamas Beszedics <beszedicshun@gmail.com>
2021-09-07 20:27:00 +0200

    Initial commit

---
+++ src/main/java/com/project/dietetics/DieteticsApplication.java
@@ -0,0 +1,13 @@
+package com.project.dietetics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieteticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieteticsApplication.class, args);
+	}
+
+}
