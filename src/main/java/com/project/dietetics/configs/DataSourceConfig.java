d9eef6892e4dc2db927791ec71e66c94fc3922de
Tamas Beszedics <beszedicshun@gmail.com>
2021-09-07 20:40:00 +0200

    Add DataSource config for db connection

---
+++ src/main/java/com/project/dietetics/configs/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.project.dietetics.configs;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://localhost:3306/dieteticsdb")
+                .username("root")
+                .password("root1234")
+                .build();
+    }
+}
