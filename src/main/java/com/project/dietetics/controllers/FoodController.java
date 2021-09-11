8dd8fdcc39221748595cf834c043cfadb23bb320
Tamas Beszedics <beszedicshun@gmail.com>
2021-09-11 15:23:50 +0200

    Add basic React frontend structure + tailwind css

--- src/main/java/com/project/dietetics/controllers/FoodController.java
+++ src/main/java/com/project/dietetics/controllers/FoodController.java
@@ -3,15 +3,13 @@ package com.project.dietetics.controllers;
 import com.project.dietetics.services.FoodService;
 import com.project.dietetics.services.models.FoodModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/food")
+@CrossOrigin
 public class FoodController {
 
     @Autowired
