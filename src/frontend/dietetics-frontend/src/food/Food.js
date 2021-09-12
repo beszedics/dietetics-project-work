65c23570a09369467e47857a4a78109ef782157b
Tamas Beszedics <beszedicshun@gmail.com>
2021-09-12 11:47:44 +0200

    Add bootstrap, create Food card

--- src/frontend/dietetics-frontend/src/food/Food.js
+++ src/frontend/dietetics-frontend/src/food/Food.js
@@ -1,11 +1,24 @@
+import './Food.css'
+
 const Food = ({ foods }) => {
     return (
-        <div className="food">
-            {foods.map((food) => (
-                <div className="food-preview" key={food.id}>
-                    <h2>{ food.name }</h2>
-                </div>
-            ))}
+        <div className="container">
+            <div className="row g-4">
+                {foods.map((food) => (
+                    <div className="col-12 col-lg-4 col-sm-6" key={food.id}>
+                        <div className="card mt-4 mb-2">
+                            <div className="card-body rounded">
+                                <div className="card-body">
+                                    <h5 className="card-title">{ food.name }</h5>
+                                    <span className="badge rounded-pill bg-secondary">{ food.glycemicIndex }</span>
+                                    <span className="badge rounded-pill bg-secondary">{ food.histamineLevel }</span>
+                                    <span className="badge rounded-pill bg-secondary">{ food.histamineSymptomSeverity }</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
