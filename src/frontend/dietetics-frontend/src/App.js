74ba87b51c844d31133e227cb30ed51527c3d545
Tamas Beszedics <beszedicshun@gmail.com>
2021-09-12 12:01:04 +0200

    Add React Router to the project

--- src/frontend/dietetics-frontend/src/App.js
+++ src/frontend/dietetics-frontend/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Navbar from "./navbar/Navbar";
 import Home from "./home/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <div className="content">
-                <Home />
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
             </div>
-        </div>
+        </Router>
     );
 }
 
--- src/frontend/dietetics-frontend/src/navbar/Navbar.js
+++ src/frontend/dietetics-frontend/src/navbar/Navbar.js
@@ -1,12 +1,14 @@
 import './Navbar.css'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Dietetics application</h1>
             <div className="links">
-                <a href="/">Login</a>
-                <a href="/register">Register</a>
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
             </div>
         </nav>
     );
