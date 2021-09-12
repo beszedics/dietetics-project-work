081c7c129b5ca87aac38fe2115f62b91fa730751
Tamas Beszedics <beszedicshun@gmail.com>
2021-09-12 09:45:04 +0200

    Add basic Food component

--- src/frontend/dietetics-frontend/src/App.js
+++ src/frontend/dietetics-frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from "./navbar/Navbar";
+import Home from "./home/Home";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <div className="content">
+                <Home />
+            </div>
         </div>
     );
 }
