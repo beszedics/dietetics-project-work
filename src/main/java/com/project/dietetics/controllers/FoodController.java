8646eff3cb24f9fe44d3b1bdb1f2dca3946e419e
Tamas Beszedics <beszedicshun@gmail.com>
2021-10-16 14:08:05 +0200

    Clear project

--- src/main/java/com/project/dietetics/controllers/FoodController.java
+++ src/main/java/com/project/dietetics/controllers/FoodController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/food")
+@RequestMapping("/api/v1/foods")
 @CrossOrigin
 public class FoodController {
 
